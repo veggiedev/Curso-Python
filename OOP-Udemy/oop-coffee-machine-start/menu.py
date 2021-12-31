1ac6871cbe4e1d4942533fffd214dc2a6ed440c4
veggiedev <veggiedev@hotmail.com>
2021-12-31 10:03:41 +0100

    nuevo ejercicio

--- OOP-Udemy/oop-coffee-machine-start/menu.py
+++ OOP-Udemy/oop-coffee-machine-start/menu.py
@@ -49,8 +49,59 @@ de = Designer('Brian', 43, 4000)
 # alu1 = Alumno('Tony Stark', 32, 'Txomin Aguirre', Colegio.clases[0])
 # print(prof1.clase)
 # print(alu1.clase)
-
-
-
-
 #print(Colegio.clases[0])
+
+
+class SoftwareEngineer:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self._salary = None
+        self._num_bugs_solved = 0
+    
+    def code(self):
+        self._num_bugs_solved += 1
+    
+    def get_salary(self):
+        if self._salary == None:
+            print("You haven't specified a valid amount yet.")
+        else:
+            print(self._salary)
+    
+    global set_your_salary
+    
+    def set_salary(self, set_your_salary):
+        if set_your_salary < 1000:
+            self._salary = 1000
+        elif set_your_salary > 20000:
+            print('You introduced an amount considered too high')  
+        else:
+            self._salary = set_your_salary
+
+se = SoftwareEngineer('Max', 56)
+
+while True:
+    try:
+        set_your_salary = int(input('Set your salary: '))
+        se.set_salary(set_your_salary)
+    except ValueError:
+            print('Enter a valid amount please.')
+            continue
+    break
+
+
+
+
+while True:
+    ask_salary = input("Get your salary? Answer 'yes' or 'no' ")
+    if ask_salary.lower() == 'yes':
+        se.get_salary()
+    elif ask_salary.lower() == 'no':
+        print('OK')
+    else:
+        print('Enter a valid answer please.')
+        continue
+    break
+        
+
+#print(se.age, se.name, se._salary)
