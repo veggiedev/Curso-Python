f377bf8e13faf78f986fd44f98838ccf1615e5cb
veggiedev <veggiedev@hotmail.com>
2022-10-06 09:57:45 +0200

    2.0.3

---
+++ Udemy_Python/Udemy_Django/javascript.js
@@ -0,0 +1,6 @@
+var pounds = prompt("What's the weight in pounds(lbs)?");
+var kilograms = pounds / 2.2046;
+pounds
+var result = alert("This is the weight in kilograms: "+ kilograms);
+result
+console.log("Conversion Completed")
