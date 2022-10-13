e3ad40f035a3a01a7b2947d07dfc539e42b6ff52
veggiedev <veggiedev@hotmail.com>
2022-10-13 10:59:18 +0200

    2.0.3

--- Udemy_Python/Udemy_Django/javascript.js
+++ Udemy_Python/Udemy_Django/javascript.js
@@ -1,6 +1,84 @@
-var pounds = prompt("What's the weight in pounds(lbs)?");
-var kilograms = pounds / 2.2046;
-pounds
-var result = alert("This is the weight in kilograms: "+ kilograms);
-result
-console.log("Conversion Completed")
+import { useContext, useState, useEffect } from "react";
+import Link from "next/link";
+//import { useUser } from "../authentication/hooks";
+
+import { UserContext } from "../contexts/UserContext";
+
+export default function Navbar(props) {
+  //const [userLoggedIn, setUserLoggedIn] = useState(false);
+  const { user, setUser } = useContext(UserContext);
+
+  async function handleLogout() {
+    await fetch("/api/logout");
+    setUser(() => false);
+    //mutate({ user: null });
+  }
+
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          {user.user ? (
+            <>
+              <li>
+                <Link href="/profile">
+                  <a>Profile</a>
+                </Link>
+              </li>
+              <li>
+                <a role="button" onClick={handleLogout}>
+                  Logout
+                </a>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link href="/signup">
+                  <a>Sign up</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/login">
+                  <a>Login</a>
+                </Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
+      <style jsx>{`
+        nav {
+          max-width: 42rem;
+          margin: 0 auto;
+          padding: 0.2rem 1.25rem;
+        }
+        ul {
+          display: flex;
+          list-style: none;
+          margin-left: 0;
+          padding-left: 0;
+        }
+        li {
+          margin-right: 1rem;
+        }
+        li:first-child {
+          margin-left: auto;
+        }
+        a {
+          color: #fff;
+          text-decoration: none;
+          cursor: pointer;
+        }
+        header {
+          color: #fff;
+          background-color: #333;
+          `}</style>
+          </header>
+        );
+      }
