56d79705195cf677c49e736cdb06b14cd2498ba8
veggiedev <veggiedev@hotmail.com>
2023-03-07 13:26:18 +0100

    menu button

--- Django/buscoAmigos/buscoAmigos/settings.py
+++ Django/buscoAmigos/buscoAmigos/settings.py
@@ -142,4 +142,4 @@ MEDIA_URL = 'templates/buscoAmigosApp/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'users/sugerencias'
